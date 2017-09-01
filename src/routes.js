2e26c02fe117fef02a8f9c7f3d4082fd120d14af
lisanne <lisanne.chin@gmail.com>
2017-09-01 18:15:50 +0200

    Setup Routing

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+
+
+// IndexRoute will reference HomePage if just / will load Homepage if /about will load AboutPage (wrapped all in the Route)
+// App component will always be loaded and nest other items, pass them as childern based on the routing (see App.js this.props.children)
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="about" component={AboutPage} />
+  </Route>
+);
