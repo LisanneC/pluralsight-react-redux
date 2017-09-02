8558f28e51b97e1c888c93ca7cca9efb8a722fc3
lisanne <lisanne.chin@gmail.com>
2017-09-02 11:44:45 +0200

    Create form onClickSave function

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -5,14 +5,21 @@ class CoursesPage extends React.Component {
     super(props, context);
 
     this.state = {
-      course: { title: null}
+      course: { title: ""}
     };
+
+  this.onTitleChange = this.onTitleChange.bind(this);
+  this.onClickSave = this.onClickSave.bind(this);
   }
 
   onTitleChange(event) {
     const course = this.state.course;
     course.title = event.target.value;
     this.setState({course: course});
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
   }
 
   render() {
@@ -22,7 +29,7 @@ class CoursesPage extends React.Component {
         <h2>Add Course</h2>
         <input
           type="text"
-          onChange={this.onTitleChange}
+          onChange={this.onTitleChange} // didnt use onChange={this.onTitleChange.bind(this)} (see bind in constructor above) when use bind in render will impact performance. Causes a new function to be created on each render.
           value={this.state.course.title} />
 
         <input
