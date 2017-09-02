7722408dab480179dc376a79ee25c6e7eea10379
lisanne <lisanne.chin@gmail.com>
2017-09-02 23:10:06 +0200

    removing add course form from coursesPage and add later to new manageCourses component

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,23 +6,6 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: ""}
-    };
-
-  this.onTitleChange = this.onTitleChange.bind(this);
-  this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course); //dispatching a action
   }
 
 courseRow(course, index) {
@@ -34,16 +17,6 @@ courseRow(course, index) {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange} // didnt use onChange={this.onTitleChange.bind(this)} (see bind in constructor above) when use bind in render will impact performance. Causes a new function to be created on each render.
-          value={this.state.course.title} />
-
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
       </div>
     );
   }
