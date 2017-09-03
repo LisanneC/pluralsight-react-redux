a1da861edac4a13a367d7c10fc6e69b3705b203d
lisanne <lisanne.chin@gmail.com>
2017-09-03 10:49:16 +0200

    Create Manage Course Page

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
-
+import ManageCoursePage from './components/course/ManageCoursePage';
 
 // IndexRoute will reference HomePage if just / will load Homepage if /about will load AboutPage (wrapped all in the Route)
 // App component will always be loaded and nest other items, pass them as childern based on the routing (see App.js this.props.children)
@@ -13,6 +13,8 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCoursePage} />
+    <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
