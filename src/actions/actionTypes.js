db3f9bc880bb3301c73dd67affaa2c6d255667f9
lisanne <lisanne.chin@gmail.com>
2017-09-11 17:06:04 +0200

    Create Save Course Thunk

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,3 +1,5 @@
 // using constant to avoid typos
 export const LOAD_COURSES_SUCCES = 'LOAD_COURSES_SUCCES';
 export const LOAD_AUTHORS_SUCCES = 'LOAD_AUTHORS_SUCCES';
+export const CREATE_COURSE_SUCCES = 'CREATE_COURSE_SUCCES';
+export const UPDATE_COURSE_SUCCES = 'UPDATE_COURSE_SUCCES';
--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,10 +5,29 @@ export function loadCoursesSucces(courses) {
   return { type: types.LOAD_COURSES_SUCCES, courses};
 }
 
+export function createCourseSucces(courses) {
+  return {type: types.CREATE_COURSE_SUCCES, courses};
+}
+
+export function updateCourseSucces(courses) {
+  return { type: types.UPDATE_COURSE_SUCCES, courses};
+}
+
 export function loadCourses() {
   return function(dispatch) {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSucces(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) { // getState access the Redux store and get particular pieces of state that you need to work within the thunk
+    return courseApi.saveCourse().then(saveCourse => {
+      course.id? dispatch(updateCourseSucces(savedCourse)): //if its true will be updating the course
+      dispatch(createCourseSucces(savedCourse));
     }).catch(error => {
       throw(error);
     });
